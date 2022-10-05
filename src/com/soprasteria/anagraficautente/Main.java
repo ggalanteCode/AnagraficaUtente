b118eb424a6ff54f10c9b07e407896f270d3c644
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 15:39:29 +0200

    inserimento dati

--- src/com/soprasteria/anagraficautente/Main.java
+++ src/com/soprasteria/anagraficautente/Main.java
@@ -6,11 +6,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nome, cognome, luogoNascita;
+		String nome, cognome, luogoNascita, patente = "", genere = "";
 		int eta;
 		double altezza;
 		boolean hasPatente;
-		char genere;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Inserisci il nome: ");
+		nome = s.nextLine();
+		System.out.print("Inserisci il cognome: ");
+		cognome = s.nextLine();
+		System.out.print("Inserisci il luogo di nascita: ");
+		luogoNascita = s.nextLine();
+		System.out.print("Inserisci l'età: ");
+		eta = s.nextInt();
+		System.out.print("Inserisci l'altezza: ");
+		altezza = s.nextDouble();
+		System.out.print("Hai la patente? (true/false): ");
+		hasPatente = s.nextBoolean();
+		System.out.print("Inserisci il genere: ");
+		genere = s.nextLine();
 	}
 
 }
