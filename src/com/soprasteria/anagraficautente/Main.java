1f683c9bbb80f5dc5a3792d6a40d7e3a3a904b37
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 16:09:07 +0200

    visualizzazione dati

--- src/com/soprasteria/anagraficautente/Main.java
+++ src/com/soprasteria/anagraficautente/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nome, cognome, luogoNascita, genere = "";
+		String nome, cognome, luogoNascita, genere;
 		int eta;
 		double altezza;
 		boolean hasPatente;
@@ -25,9 +25,17 @@ public class Main {
 		altezza = s.nextDouble();
 		System.out.print("Hai la patente? (true/false): ");
 		hasPatente = s.nextBoolean();
+		s.nextLine();
 		System.out.print("Inserisci il genere: ");
 		genere = s.nextLine();
 		
+		System.out.printf("Nome: %s\n", nome);
+		System.out.printf("Cognome: %s\n", cognome);
+		System.out.printf("Luogo di nascita: %s\n", luogoNascita);
+		System.out.printf("Età: %d\n", eta);
+		System.out.printf("Altezza: %.2f\n", altezza);
+		System.out.printf("Patente: %s\n", String.valueOf(hasPatente));
+		System.out.printf("Genere: %s\n", genere);
 	}
 
 }
