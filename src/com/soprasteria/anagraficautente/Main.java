917adeb2795e6947095891b2194776ff970270d6
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 15:23:32 +0200

    aggiunte le variabili relative all'anagrafica

--- src/com/soprasteria/anagraficautente/Main.java
+++ src/com/soprasteria/anagraficautente/Main.java
@@ -1,10 +1,16 @@
 package com.soprasteria.anagraficautente;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String nome, cognome, luogoNascita;
+		int eta;
+		double altezza;
+		boolean hasPatente;
+		char genere;
 	}
 
 }
